57662d5e22fbe56c7e8e5218b1cbfabdcaad801c
Antti Pollanen <antti.pollanen89@gmail.com>
2026-03-04 18:51:01 +0200

    started 09/00

---
+++ cpp09/ex00/incl/BitcoinExchange.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+class BitcoinExchange {
+	private:
+	public:
+		BitcoinExchange(void) = delete;
+		BitcoinExchange(const BitcoinExchange &other) = delete;
+		BitcoinExchange &operator=(const BitcoinExchange &other) = delete;
+		~BitcoinExchange(void) = delete;
+};
