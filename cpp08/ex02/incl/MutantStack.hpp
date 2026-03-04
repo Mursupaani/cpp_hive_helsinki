1e3532f6866832d5bd036fdce96ea57c460d7860
Antti Pollanen <antti.pollanen89@gmail.com>
2026-03-04 16:09:28 +0200

    Finished cpp08/ex02

--- cpp08/ex02/incl/MutantStack.hpp
+++ cpp08/ex02/incl/MutantStack.hpp
@@ -1,14 +1,47 @@
 #pragma once
-#include <deque>
-#include <iostream>
 #include <stack>
-#include <string>
 
-template <typename T>
-class MutantStack : public std::stack<T> {
+template <typename T, typename Container = std::deque<T>>
+class MutantStack : public std::stack<T, Container> {
 	public:
-		MutantStack<T>(void);
-		MutantStack<T>(const MutantStack<T> &other);
-		MutantStack<T> &operator=(const MutantStack<T> &other);
-		~MutantStack<T>(void);
+		using StackType = typename std::stack<T, Container>;
+		using iterator = typename Container::iterator;
+		using const_iterator = typename Container::const_iterator;
+		using reverse_iterator = typename Container::reverse_iterator;
+		using const_reverse_iterator =
+			typename Container::const_reverse_iterator;
+		MutantStack(void) {};
+		MutantStack(const MutantStack<T> &other) : StackType(other) {};
+		MutantStack &operator=(const MutantStack<T, Container> &other) {
+			if (this == &other)
+				return (*this);
+			this->StackType::operator=(other);
+			return (*this);
+		};
+		~MutantStack(void) {};
+
+		iterator begin() {
+			return (this->c.begin());
+		}
+		iterator end() {
+			return (this->c.end());
+		}
+		const_iterator cbegin() const {
+			return (this->c.cbegin());
+		}
+		const_iterator cend() const {
+			return (this->c.cend());
+		}
+		reverse_iterator rbegin() {
+			return (this->c.rbegin());
+		}
+		reverse_iterator rend() {
+			return (this->c.rend());
+		}
+		const_reverse_iterator crbegin() const {
+			return (this->c.crbegin());
+		}
+		const_reverse_iterator crend() const {
+			return (this->c.crend());
+		}
 };
--- cpp08/ex02/src/main.cpp
+++ cpp08/ex02/src/main.cpp
@@ -10,6 +10,147 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <vector>
+
 #include "MutantStack.hpp"
 
-int main(void) {}
+int main(void) {
+	{
+		std::cout << "TEST 1 (from subject):\n\n";
+		MutantStack<int> mstack;
+		mstack.push(5);
+		mstack.push(17);
+		std::cout << mstack.top() << std::endl;
+		mstack.pop();
+		std::cout << mstack.size() << std::endl;
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+		//[...]
+		mstack.push(0);
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+		++it;
+		--it;
+		while (it != ite) {
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::stack<int> s(mstack);
+		std::cout << "____________________\n\n";
+	}
+	{
+		std::cout << "TEST 2:\n\n";
+		MutantStack<int> mstack;
+		mstack.push(5);
+		mstack.push(17);
+		MutantStack<int> mstack_copy(mstack);
+		std::cout << "Original contents:\n";
+		for (auto &elem : mstack)
+			std::cout << elem << std::endl;
+		std::cout << "\nCopy contents:\n";
+		for (auto &elem : mstack_copy)
+			std::cout << elem << std::endl;
+		std::cout << "\nPop one from original\n";
+		mstack.pop();
+		std::cout << "\nOriginal contents:\n";
+		for (auto &elem : mstack)
+			std::cout << elem << std::endl;
+		std::cout << "\nCopy contents:\n";
+		for (auto &elem : mstack_copy)
+			std::cout << elem << std::endl;
+		std::cout << "____________________\n\n";
+	}
+	{
+		std::cout << "TEST 3:\n\n";
+		MutantStack<int> mstack;
+		mstack.push(5);
+		mstack.push(17);
+		MutantStack<int> mstack_copy;
+		mstack_copy = mstack;
+		std::cout << "Original contents:\n";
+		for (auto &elem : mstack)
+			std::cout << elem << std::endl;
+		std::cout << "\nCopy contents:\n";
+		for (auto &elem : mstack_copy)
+			std::cout << elem << std::endl;
+		std::cout << "\nPop one from original\n";
+		mstack.pop();
+		std::cout << "\nOriginal contents:\n";
+		for (auto &elem : mstack)
+			std::cout << elem << std::endl;
+		std::cout << "\nCopy contents:\n";
+		for (auto &elem : mstack_copy)
+			std::cout << elem << std::endl;
+		std::cout << "____________________\n\n";
+	}
+	{
+		std::cout << "TEST 4:\n\n";
+		MutantStack<int> mstack;
+		mstack.push(5);
+		mstack.push(4);
+		mstack.push(3);
+		mstack.push(2);
+		mstack.push(1);
+		std::cout << "Iterator:\n";
+		for (auto it = mstack.begin(); it != mstack.end(); ++it)
+			std::cout << *it << std::endl;
+		std::cout << "\nReverse iterator:\n";
+		for (auto it = mstack.rbegin(); it != mstack.rend(); ++it)
+			std::cout << *it << std::endl;
+		std::cout << "____________________\n\n";
+	}
+	{
+		std::cout << "TEST 5:\n\n";
+		MutantStack<int> mstack;
+		mstack.push(5);
+		mstack.push(4);
+		mstack.push(3);
+		mstack.push(2);
+		mstack.push(1);
+		const MutantStack<int> const_mstack(mstack);
+		std::cout << "Const iterator:\n";
+		for (auto it = const_mstack.cbegin(); it != const_mstack.cend(); ++it)
+			std::cout << *it << std::endl;
+		std::cout << "\nConst reverse iterator:\n";
+		for (auto it = const_mstack.crbegin(); it != const_mstack.crend(); ++it)
+			std::cout << *it << std::endl;
+		std::cout << "____________________\n\n";
+	}
+	{
+		std::cout << "TEST 5 (using vector and strings):\n\n";
+		MutantStack<std::string, std::vector<std::string>> mstack;
+		mstack.push("Test 1");
+		mstack.push("Test 2");
+		mstack.push("Test 3");
+		mstack.push("Test 4");
+		mstack.push("Test 5");
+		std::cout << "Iterator:\n";
+		for (auto it = mstack.begin(); it != mstack.end(); ++it)
+			std::cout << *it << std::endl;
+		std::cout << "\nReverse iterator:\n";
+		for (auto it = mstack.rbegin(); it != mstack.rend(); ++it)
+			std::cout << *it << std::endl;
+		std::cout << "____________________\n\n";
+	}
+	{
+		std::cout << "TEST 6 (using list and float):\n\n";
+		MutantStack<float, std::list<float>> mstack;
+		mstack.push(0.1f);
+		mstack.push(0.2f);
+		mstack.push(0.3f);
+		mstack.push(0.4f);
+		mstack.push(0.5f);
+		std::cout << "Iterator:\n";
+		for (auto it = mstack.begin(); it != mstack.end(); ++it)
+			std::cout << *it << std::endl;
+		std::cout << "\nReverse iterator:\n";
+		for (auto it = mstack.rbegin(); it != mstack.rend(); ++it)
+			std::cout << *it << std::endl;
+		std::cout << "____________________\n\n";
+	}
+	return 0;
+}
