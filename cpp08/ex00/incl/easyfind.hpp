bd5e72a89eb1c74202d348d12cd760c6fde5012c
Antti Pollanen <antti.pollanen89@gmail.com>
2026-03-04 16:36:16 +0200

    Fixed 08/00

--- cpp08/ex00/incl/easyfind.hpp
+++ cpp08/ex00/incl/easyfind.hpp
@@ -4,17 +4,17 @@
 #include <string>
 
 template <typename T>
-auto &easyfind(T &container, int i) {
-	auto it = std::find(container.begin(), container.end(), i);
+auto &easyfind(T &container, int find) {
+	auto it = std::find(container.begin(), container.end(), find);
 	if (it == container.end())
-		throw std::runtime_error(std::to_string(i) + " not found");
+		throw std::runtime_error(std::to_string(find) + " not found");
 	return (*it);
 }
 
 template <typename T>
-const auto &easyfind(const T &container, int i) {
-	auto it = std::find(container.cbegin(), container.cend(), i);
+const auto &easyfind(const T &container, int find) {
+	auto it = std::find(container.cbegin(), container.cend(), find);
 	if (it == container.cend())
-		throw std::runtime_error(std::to_string(i) + " not found");
+		throw std::runtime_error(std::to_string(find) + " not found");
 	return (*it);
 }
--- cpp08/ex00/src/main.cpp
+++ cpp08/ex00/src/main.cpp
@@ -24,6 +24,18 @@ int main(void) {
 		std::array<int, 5> array = {0, 1, 2, 3, 4};
 		std::cout << easyfind(array, 0) << std::endl;
 		try {
+			std::cout << "\nLooking for 3\n";
+			std::cout << easyfind(array, 3) << " found" << std::endl;
+		} catch (std::runtime_error &e) {
+			std::cout << e.what() << std::endl;
+		}
+		std::cout << "____________________\n\n";
+	}
+	{
+		std::cout << "TEST 2 (std::array):\n";
+		std::array<int, 5> array = {0, 1, 2, 3, 4};
+		std::cout << easyfind(array, 0) << std::endl;
+		try {
 			std::cout << "\nLooking for 5\n";
 			std::cout << easyfind(array, 5) << std::endl;
 		} catch (std::runtime_error &e) {
@@ -32,7 +44,7 @@ int main(void) {
 		std::cout << "____________________\n\n";
 	}
 	{
-		std::cout << "TEST 2 (std::vector):\n";
+		std::cout << "TEST 3 (std::vector):\n";
 		std::vector<int> vector = {0, 1, 2, 3, 4};
 		std::cout << easyfind(vector, 1) << std::endl;
 		try {
@@ -44,7 +56,7 @@ int main(void) {
 		std::cout << "____________________\n\n";
 	}
 	{
-		std::cout << "TEST 3 (std::list):\n";
+		std::cout << "TEST 4 (std::list):\n";
 		std::list<int> vector = {0, 1, 2, 3, 4};
 		std::cout << "Looking for 2\n";
 		std::cout << easyfind(vector, 2) << std::endl;
@@ -57,7 +69,7 @@ int main(void) {
 		std::cout << "____________________\n\n";
 	}
 	{
-		std::cout << "TEST 4 (std::array):\n";
+		std::cout << "TEST 5 (std::array):\n";
 		std::array<int, 5> array = {0, 1, 2, 3, 4};
 		int				  &x = easyfind(array, 0);
 		x = 10;
@@ -65,22 +77,22 @@ int main(void) {
 			std::cout << val << std::endl;
 		try {
 			std::cout << "\nLooking for 10\n";
-			std::cout << easyfind(array, 10) << std::endl;
+			std::cout << easyfind(array, 10) << " found" << std::endl;
 		} catch (std::runtime_error &e) {
 			std::cout << e.what() << std::endl;
 		}
 		std::cout << "____________________\n\n";
 	}
 	{
-		std::cout << "TEST 5 (const std::array):\n";
+		std::cout << "TEST 6 (const std::array):\n";
 		const std::array<int, 5> array = {0, 1, 2, 3, 4};
 		// int &x = easyfind(array, 0);
 		// x = 10;
 		for (auto val : array)
 			std::cout << val << std::endl;
 		try {
-			std::cout << "\nLooking for 10\n";
-			std::cout << easyfind(array, 10) << std::endl;
+			std::cout << "\nLooking for 4\n";
+			std::cout << easyfind(array, 4) << " found" << std::endl;
 		} catch (std::runtime_error &e) {
 			std::cout << e.what() << std::endl;
 		}
